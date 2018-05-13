01f0b3a2cce27808e34adc94ee4e4d57dc4f210f
Achal Agarwal <f2015436@pilani.bits-pilani.ac.in>
2018-05-13 12:43:36 +0530

    Fast Scanner Class

---
+++ IDEA/src/Ex1/FastScanner.java
@@ -0,0 +1,22 @@
+package Ex1;
+import java.util.*;
+import java.io.*;
+
+public class FastScanner {
+    StringTokenizer tok = new StringTokenizer("");
+     BufferedReader in;
+
+    FastScanner() {
+        in = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    String next() throws IOException {
+        while (!tok.hasMoreElements())
+            tok = new StringTokenizer(in.readLine());
+        return tok.nextToken();
+    }
+
+    int nextInt() throws IOException {
+        return Integer.parseInt(next());
+    }
+    }
